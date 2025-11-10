227edcf32226781bb17033a45fb8f992e833566f
Ducld <ducld@toprate.io>
2025-11-10 11:54:14 +0700

    refactor: config vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Entry-Test/',
-  test: {
-    environment: 'jsdom',
-    include: ['**/*.test.tsx'],
-  }
+  // test: {
+  //   environment: 'jsdom',
+  //   include: ['**/*.test.tsx'],
+  // }
 })
