2f1f1217a776aebb0d9f4db2ec93d8f03d7f5cb9
Ducld <ducld@toprate.io>
2025-11-10 11:37:23 +0700

    fix: deploy error

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/Entry-Test/',
   test: {
     environment: 'jsdom',
     include: ['**/*.test.tsx'],
